2ac41bcebdfefb7edab3a654ec9d34a62e83ee4e
urayarvia <uray.arvia1838@gmail.com>
2021-09-16 16:47:43 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Uray Arvia Maharani. Saya Tinggal Di Kota Pontianak. Hobi saya adalah Menari.";
+        
+    }
+}
